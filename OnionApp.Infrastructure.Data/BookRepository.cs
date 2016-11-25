4e16d9d9204504f925a041777f0dd0e6fbc5905c
Aktore <aktore.niyazymbetov@gmail.com>
2016-11-25 17:10:44 +0600

    Архитектура понятная но есть ошибки в связке BookRepository

---
+++ OnionApp.Infrastructure.Data/BookRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using OnionApp.Domain.Interfaces;
+using OnionApp.Domain.Core;
+
+
+namespace OnionApp.Infrastructure.Data
+{
+    class BookRepository : IBookRepository
+    {
+        private OrderContext db;
+
+        public BookRepository()
+        {
+            this.db = new OrderContext();
+        }
+        public IEnumerable<Book> GetBookList()
+        {
+            return db.Books.ToList();
+        }
+        public void Create(Book book)
+        {
+            db.Books.Add(book);
+        }
+
+        public void Delete(int Id)
+        {
+            Book book = db.Books.Find(Id);
+            if (book != null)
+                db.Books.Remove(book);
+        }
+
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Book GetBook(int Id)
+        {
+            return db.Books.Find(Id);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+        }
+    }
+}
---
+++ OnionApp.Infrastructure.Data/OrderContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using OnionApp.Domain.Core;
+using System.Data.Entity;
+
+namespace OnionApp.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
